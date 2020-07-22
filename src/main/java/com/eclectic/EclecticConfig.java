af6e7ce5c37e4dc9ecd32c09822f2e9229972a77
zenpj <qanobie3@gmail.com>
2020-07-22 17:09:46 +1000

    Updated README

--- src/main/java/com/eclectic/EclecticConfig.java
+++ src/main/java/com/eclectic/EclecticConfig.java
@@ -7,13 +7,5 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("example")
 public interface EclecticConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
-	)
-	default String greeting()
-	{
-		return "Hello";
-	}
+
 }
